4c98baf0563634568ede492353dc21b80d053ebe
Jorge Barnaby <jorge.barnaby@gmail.com>
2023-01-07 12:32:06 -0500

    Add json-db with lowdb

--- src/config.js
+++ src/config.js
@@ -17,6 +17,7 @@ export const config = {
   },
   baseUrl: env.get('BASE_URL').default('https://www.fpl.com/').asString(),
   dataDirectory: env.get('DATA_DIR').default('data').asString(),
+  jsonDbFile: env.get('JSON_DB_FILE').default('db.json').asString(),
   nodeEnv,
   isProduction: nodeEnv === 'PRODUCTION',
 };
---
+++ src/json-db.js
@@ -0,0 +1,72 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Low } from 'lowdb';
+import lodash from 'lodash';
+import { JSONFile } from 'lowdb/node';
+import { config } from './config.js';
+import { createLogger } from './logger.js';
+
+const logger = createLogger('json-db');
+
+const initJsonDb = async () => {
+  // File path
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const file = join(__dirname, config.jsonDbFile);
+
+  // Configure lowdb to write to JSONFile
+  const adapter = new JSONFile(file);
+
+  // Extend Low class with a new `chain` field
+  class LowWithLodash extends Low {
+    chain = lodash.chain(this).get('data');
+  }
+
+  const jsonDb = new LowWithLodash(adapter);
+
+  // Read data from JSON file, this will set db.data content
+  await jsonDb.read();
+
+  const initialData = {
+    hourlyUsage: { records: [] },
+    dailyUsage: { records: [] },
+    monthlyUsage: { records: [] },
+  };
+
+  // If db.json doesn't exist, db.data will be null
+  // So we set an initial structure
+  jsonDb.data ||= initialData;
+
+  return jsonDb;
+};
+
+const jsonDb = await initJsonDb();
+
+const saveUsageRecord = async (type, id, record) => {
+  if (!jsonDb.data[type]) {
+    throw new Error(`Usage type ${type} is not valid`);
+  }
+
+  // value() must be called to execute chain
+  const existingRecord = jsonDb.chain
+    .get([type, 'records'])
+    .find({ _id: id })
+    .value();
+
+  if (existingRecord) {
+    logger.debug({ id }, `${type} record already exists`);
+    return;
+  }
+
+  jsonDb.data[type].records.push(record);
+
+  await jsonDb.write();
+};
+
+export const saveHourlyUsage = async (id, record) =>
+  saveUsageRecord('hourlyUsage', id, record);
+
+export const saveDailyUsage = async (id, record) =>
+  saveUsageRecord('dailyUsage', id, record);
+
+export const saveMonthlyUsage = async (id, record) =>
+  saveUsageRecord('monthlyUsage', id, record);
